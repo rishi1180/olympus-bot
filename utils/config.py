2171f98abe7034d1ecc7b6d84423cc93fd93d0ae
rishi1180 <samsung123.cph@gmail.com>
2026-02-05 06:49:36 +0530

    config.py

--- utils/config.py
+++ utils/config.py
@@ -1,9 +1,9 @@
 import os
 
 TOKEN = os.environ.get("TOKEN")
-NAME = "Olympus"
-server = "https://discord.com/invite/odx"
+NAME = "Pixel Node"
+server = "https://discord.gg/jFxXSFwY45"
 ch = "https://discord.com/channels/699587669059174461/1271825678710476911"
-OWNER_IDS = [213347081799073793, 1070619070468214824]
-BotName = "Olympus"
-serverLink = "https://discord.com/invite/odx"
+OWNER_IDS = [1306637704552906873,]
+BotName = "PixelNode"
+serverLink = "https://discord.gg/jFxXSFwY45"
